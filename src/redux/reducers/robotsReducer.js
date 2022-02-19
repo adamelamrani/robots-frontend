f85908ac7999923a5e6eaa2c2964ab071fd44415
Adam El Amrani <adelamco1091@outlook.com>
2022-02-19 17:45:12 +0100

    Tested all switch cases for robotsReducer

--- src/redux/reducers/robotsReducer.js
+++ src/redux/reducers/robotsReducer.js
@@ -2,18 +2,20 @@ import actionTypes from "../actions/actionTypes/actionTypes";
 
 const robotsReducer = (robots = [], action = {}) => {
   let newRobotsState;
-  switch (action.types) {
+  switch (action.type) {
     case actionTypes.loadRobots:
       newRobotsState = [...action.robots];
       break;
     case actionTypes.createRobot:
-      newRobotsState = [...robots, ...action.robots];
+      newRobotsState = [...robots, ...action.robot];
       break;
     case actionTypes.updateRobot:
-      newRobotsState = robots.filter((robot) => action.id === robot.id);
+      newRobotsState = robots.map((robot) =>
+        robot.id === action.robot.id ? { ...action.robot } : { ...robot }
+      );
       break;
     case actionTypes.deleteRobot:
-      newRobotsState = robots.filter((robot) => action.id !== robot.id);
+      newRobotsState = robots.filter((robot) => robot.id !== action.id);
       break;
     default:
       newRobotsState = [...robots];
