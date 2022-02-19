8964e08246a9c67b4351665385ca4958fdbb9184
Adam El Amrani <adelamco1091@outlook.com>
2022-02-19 14:44:45 +0100

    Tested action creators

--- src/redux/actions/actionsCreator/actionsCreator.js
+++ src/redux/actions/actionsCreator/actionsCreator.js
@@ -1,21 +1,21 @@
 import actionTypes from "../actionTypes/actionTypes";
 
-const loadRobotsAction = (robots) => ({
+export const loadRobotsAction = (robots) => ({
   type: actionTypes.loadRobots,
   robots,
 });
 
-const createRobotAction = (robot) => ({
+export const createRobotAction = (robot) => ({
   type: actionTypes.createRobot,
   robot,
 });
 
-const updateRobotAction = (id) => ({
+export const updateRobotAction = (id) => ({
   type: actionTypes.updateRobot,
   id,
 });
 
-const deleteRobotAction = (id) => ({
+export const deleteRobotAction = (id) => ({
   type: actionTypes.deleteRobot,
   id,
 });
