94645d447399566ffb73aaa1b2d23508d96943ae
Adam El Amrani <adelamco1091@outlook.com>
2022-02-19 12:25:01 +0100

    Created robotspagecomponent and it's styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavComponent from "./components/NavComponent/NavComponent";
+import RobotsPageComponent from "./components/RobotsPageComponent/RobotsPageComponent";
 
 function App() {
   return (
     <>
       <NavComponent />
       <h1>Robots</h1>
+      <RobotsPageComponent />
     </>
   );
 }
---
+++ src/components/RobotsPageComponent/RobotsPageComponent.js
@@ -0,0 +1,12 @@
+import { RobotsPageStyled, RobotsPageHeadingStyled } from "./RobotsPageStyled";
+
+const RobotsPageComponent = () => {
+  return (
+    <>
+      <RobotsPageHeadingStyled>Robots List</RobotsPageHeadingStyled>
+      <RobotsPageStyled></RobotsPageStyled>
+    </>
+  );
+};
+
+export default RobotsPageComponent;
