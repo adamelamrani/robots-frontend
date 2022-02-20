b0b4f33cb37018f2de3013ef5bd17a201af4a9a9
adam-elamrani <adelamco1091@outlook.com>
2022-02-20 11:59:58 +0100

    Imported browser router in index.js, added routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import NavComponent from "./components/NavComponent/NavComponent";
 import RobotsPageComponent from "./components/RobotsPageComponent/RobotsPageComponent";
 
 function App() {
   return (
     <>
-      <NavComponent />
+     <NavComponent />
       <h1>Robots</h1>
       <RobotsPageComponent />
+      <Routes>
+        <Route path="/*" element={<RobotsPageComponent />}></Route>
+      </Routes>
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store/store";
 
@@ -7,9 +8,11 @@ describe("Given an app component", () => {
   describe("When it's rendered", () => {
     test("Then it should render a heading with 'Robot's text", () => {
       render(
+        <BrowserRouter>
         <Provider store={store}>
           <App />
         </Provider>
+        </BrowserRouter>
       );
       const screenText = screen.getByRole("heading", { name: "Robots" });
       expect(screenText).toBeInTheDocument();
